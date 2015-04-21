948c485a6d91b5edeb1631f84bea8ca02dfc19ed
Ian MacDonald & Toby Alden <student@epicodus.com>
2015-04-21 11:39:08 -0700

    Fail third spec

--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -5,7 +5,10 @@ describe('Fixnum#numbers_to_words') do
   it("Given a Fixnum between 0 and 9, the method will return the written equivalent") do
     expect(1.numbers_to_words()).to(eq('one'))
   end
-  it("Given a Fixnum between 0 and 99, the method will return the written equivalent") do
+  it("Given a Fixnum between 10 and 19, the method will return the written equivalent") do
+    expect(13.numbers_to_words()).to(eq('thirteen'))
+  end
+  it("Given a Fixnum between 20 and 99, the method will return the written equivalent") do
     expect(23.numbers_to_words()).to(eq('twenty three'))
   end
 end

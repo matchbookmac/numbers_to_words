c745962058ec3891d369a6a9d53af26ab9d0fda2
Ian MacDonald & Toby Alden <student@epicodus.com>
2015-04-21 12:28:04 -0700

    Started work on power of three modifiers

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -9,20 +9,8 @@ class Fixnum
     ones_digit = {0 => nil, 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine"}
     teens_digits = {0 => "ten", 1 => "eleven", 2 => "twelve", 3 => "thirteen", 4 => "fourteen", 5 => "fifteen", 6 => "sixteen", 7 => "seventeen", 8 => "eighteen", 9 => "nineteen"}
     tens_digit = {0 => nil, 2 => "twenty", 3 => "thirty", 4 => "fourty", 5 => "fifty", 6 => "sixty", 7 => "seventy", 8 => "eighty", 9 => "ninety"}
-
+    pow_three_places = {0 => "thousand", 2 => "million", 3 => "billion", 4 => "trillion"}
     number_digits.reverse!()
-
-
-    #number_of_pow_three_arrays = (number_digits.length()/3).floor();
-    #pow_three_array_array = []
-
-    #while(number_of_pow_three_arrays >= 0) do
-    #  pow_three_array = []
-    #  number_digits[0 + number_of_pow_three_arrays*3]
-    #  number_digits[1]
-    #  number_digits[2]
-    #  pow_three_array_array.push()
-    #end
 
     if(self.eql?(0))
       written_number.push("zero")

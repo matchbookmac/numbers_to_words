02c6a33e19ae9595cb31c55318204022f43aba03
Ian MacDonald & Toby Alden <student@epicodus.com>
2015-04-21 11:37:22 -0700

    Pass second spec

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,8 +1,23 @@
 class Fixnum
   define_method(:numbers_to_words) do
+    written_number = []
+    number_digits = self.to_s().split("")
+    number_digits.map!() do |number|
+      number.to_i()
+    end
 
-  ones_digit = {0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine"}
-  ones_digit.fetch(self)
+    ones_digit = {0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine"}
+    teens = {}
+    tens_digit = {2 => "twenty", 3 => "thirty", 4 => "fourty", 5 => "fifty", 6 => "sixty", 7 => "seventy", 8 => "eighty", 9 => "ninety"}
 
+    number_digits.reverse!()
+    number_digits.each_index() do |index|
+      if(index.eql?(0))
+        written_number.push(ones_digit.fetch(number_digits[index]))
+      elsif(index.eql?(1))
+        written_number.push(tens_digit.fetch(number_digits[index]))
+      end
+    end
+    written_number.reverse!().join(" ")
   end
 end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -5,4 +5,7 @@ describe('Fixnum#numbers_to_words') do
   it("Given a Fixnum between 0 and 9, the method will return the written equivalent") do
     expect(1.numbers_to_words()).to(eq('one'))
   end
+  it("Given a Fixnum between 0 and 99, the method will return the written equivalent") do
+    expect(23.numbers_to_words()).to(eq('twenty three'))
+  end
 end
